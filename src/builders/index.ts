1810ffe3311f83226b899c156fb036ddc94d40ab
slekup <74949101+slekup@users.noreply.github.com>
2023-05-26 01:40:29 +1200

    setup files

---
+++ src/builders/index.ts
@@ -0,0 +1,3 @@
+export { default as EndpointBuilder } from './EndpointBuilder';
+export { default as RouteBuilder } from './RouteBuilder';
+export { default as SchemaBuilder } from './SchemaBuilder';
---
+++ src/utils/middleware/wrapper.middleware.ts
@@ -0,0 +1,46 @@
+import { NextFunction, Request, Response } from 'express';
+import { ClientSession, startSession } from 'mongoose';
+
+import { logger } from '@utils/index';
+
+type ControllerType = (req: Request, res: Response, session: ClientSession) => Promise<unknown> | unknown;
+
+/**
+ * Handles a function.
+ * @param controller The handler function.
+ * @param req The request.
+ * @param res The response.
+ * @param next The next function.
+ */
+const handledFunction = async (
+  controller: ControllerType,
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<void> => {
+  const session = await startSession();
+
+  try {
+    session.startTransaction();
+    await controller(req, res, session);
+    await session.commitTransaction();
+    next();
+  } catch (error) {
+    await session.abortTransaction();
+    logger.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  } finally {
+    session.endSession();
+  }
+};
+
+/**
+ * Wraps a handler function in error handling.
+ * @param controller The handler function.
+ * @returns The handler function wrapped in error handling.
+ */
+export const withErrorHandling =
+  (controller: ControllerType) =>
+  (req: Request, res: Response, next: NextFunction): void => {
+    handledFunction(controller, req, res, next);
+  };
