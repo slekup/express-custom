172df9c2e18dfadd479888a80448a3e0e1d0ed0a
slekup <74949101+slekup@users.noreply.github.com>
2023-06-02 09:03:21 +1200

    small changes

--- site/src/components/global/Button.tsx
+++ site/src/components/global/Button.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import { useState } from 'react';
+
 type ButtonStyle = 'primary' | 'default';
 
 interface Props
@@ -11,8 +13,10 @@ interface Props
     'style'
   > {
   children: React.ReactNode;
+  className?: string;
   style?: ButtonStyle;
   size?: 'sm' | 'md' | 'lg';
+  copy?: string;
 }
 
 const styles = {
@@ -22,22 +26,54 @@ const styles = {
     'text-text-secondary hover:bg-default active:bg-default-hover border border-border',
 };
 
-export default function Button({ children, size, style, ...props }: Props) {
+export default function Button({
+  children,
+  size,
+  style,
+  className,
+  copy,
+  ...props
+}: Props) {
+  const [copied, setCopied] = useState<boolean>(false);
+  const [copyTimeout, setCopyTimeout] = useState<NodeJS.Timeout | null>(null);
+
+  const newProps = { ...props };
+
   const baseClass = `inline-block rounded-md no-select text-sm font-medium ${
-    styles[style ?? 'primary']
-  }`;
+    copied
+      ? 'bg-success text-white hover:bg-sucess-hover active:bg-success-active'
+      : styles[style ?? 'primary']
+  } ${className ?? ''}`;
+
+  const removeCopy = () => {
+    setCopied(false);
+  };
+
+  if (copy) {
+    newProps.onClick = () => {
+      navigator.clipboard.writeText(copy);
+      setCopied(true);
+
+      if (copyTimeout) {
+        clearTimeout(copyTimeout);
+      }
+
+      const timeout = setTimeout(removeCopy, 4000);
+      setCopyTimeout(timeout);
+    };
+  }
 
   return size === 'lg' ? (
-    <button className={`h-12 py-2 px-6 ${baseClass}`} {...props}>
-      {children}
+    <button className={`h-12 px-6 py-2 ${baseClass}`} {...newProps}>
+      {copied ? 'Copied' : children}
     </button>
   ) : size === 'sm' ? (
-    <button className={`h-8 py-1 px-2 ${baseClass}`} {...props}>
-      {children}
+    <button className={`h-8 px-2 py-1 ${baseClass}`} {...newProps}>
+      {copied ? 'Copied' : children}
     </button>
   ) : (
-    <button className={`h-10 py-1.5 px-4 ${baseClass}`} {...props}>
-      {children}
+    <button className={`h-10 px-4 py-1.5 ${baseClass}`} {...newProps}>
+      {copied ? 'Copied' : children}
     </button>
   );
 }
