125c1da2bc2e4bc6bf58b34862172b0325e25abb
slekup <74949101+slekup@users.noreply.github.com>
2023-06-07 18:31:48 +1200

    update pipeline & improve readability

--- src/builders/index.ts
+++
@@ -1,7 +0,0 @@
-export { default as ApiBuilder } from './ApiBuilder';
-export { default as EndpointBuilder } from './EndpointBuilder';
-export { default as RouteBuilder } from './RouteBuilder';
-export { default as RouterBuilder } from './RouterBuilder';
-export { default as SchemaBuilder } from './SchemaBuilder';
-export { default as StructureBuilder } from './StructureBuilder';
-export { default as VersionBuilder } from './VersionBuilder';
